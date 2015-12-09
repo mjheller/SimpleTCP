ac1ab4b72ddf8a397f23645925a219b9f5c77366
Matt <mjheller414@gmail.com>
2015-12-09 09:19:11 -0600

    first commit

---
+++ SimpleTCP/Data.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTCP
+{
+    class Data
+    {
+        private string senderIP;
+        private string recipientIP;
+        private string protocol;
+        private object message;
+        public string SenderIP { get {return senderIP;} set{ senderIP = value;}}
+        public string RecipientIP { get { return SenderIP; } set { SenderIP = value; } }
+        public string Protocol { get { return protocol; } set { protocol = value; } }
+        public Object Message { get { return message; } set { message = value; } }
+
+    
+        public Data(string SenderIP, string RecipientIp, string Protocol, string message)
+        {
+            this.senderIP = SenderIP;
+            this.recipientIP = RecipientIP;
+            this.protocol = Protocol;
+            this.message = Message;
+        }
+
+    }
+}
---
+++ SimpleTCP/PacketData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace SimpleTCP
+{
+    public class PacketData<T>
+    {
+        
+        public PacketData(T data)
+        {
+            
+        }
+
+        private byte[] ObjectToByteArray(Object obj)
+        {
+            if (obj == null)
+                return null;
+            BinaryFormatter bf = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bf.Serialize(ms, obj);
+                return ms.ToArray();
+            }
+
+
+
+
+        }
+}
